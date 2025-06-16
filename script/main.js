ce1f4b80cdeb44561e9888c616cd2e9fc829ddec
shakil-babu <mail.shakilbabu@gmail.com>
2025-06-16 23:03:59 +0600

    hbd

--- script/main.js
+++ script/main.js
@@ -27,11 +27,11 @@ const animationTimeline = () => {
 
     textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
         .split("")
-        .join("</span><span>")}</span>`;
+        .join("</span><span>")} </span>`;
 
     hbd.innerHTML = `<span>${hbd.innerHTML
         .split("")
-        .join("</span><span>")}</span>`;
+        .join("</span><span>")} </span>`;
 
     const ideaTextTrans = {
         opacity: 0,
